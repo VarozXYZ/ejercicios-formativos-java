c839ae274ca053f8b1c61822750bc8a28dd5a34e
VarozXYZ <alvaropiro@gmail.com>
2024-10-25 00:02:06 +0200

    añadida actividad 3

---
+++ Motor.java
@@ -0,0 +1,24 @@
+public class Motor {
+	private int cilindrada;
+	private boolean arrancado; // Por default, un boolean se autodeclara false
+	
+	public Motor(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	
+	public void cambiarMotor() {
+		if (arrancado == true) {
+			arrancado = false;
+			System.out.println("Motor apagado");
+		} else {
+			arrancado = true;
+			System.out.println("Motor encendido");
+		}
+	}
+	
+	public static void main (String[] args) {
+		Motor motor1 = new Motor(2000);
+		motor1.cambiarMotor();
+		motor1.cambiarMotor();
+	}
+}
